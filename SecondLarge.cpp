73e8f9c58b81d8c4aca3479ca08c598b21997a48
prashant404 <pkr214204@gmail.com>
2024-02-15 02:27:08 +0530

    Update MAIN

--- SecondLarge.cpp
+++ SecondLarge.cpp
@@ -1,4 +1,8 @@
-vector<int> getSecondOrderElements(int n, vector<int> a) {
+#include <iostream>
+#include <vector>
+#include <climits>
+
+std::vector<int> getSecondOrderElements(int n, std::vector<int> a) {
     if(n<2)
     {
         return {};
@@ -33,4 +37,25 @@ vector<int> getSecondOrderElements(int n, vector<int> a) {
     }
 
     return {secondLargest, secondsmallest};
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<int> a;
+  
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        std::cin >> temp;
+        a.push_back(temp);
+    }
+     std::vector<int> result = getSecondOrderElements(n,a);
+
+     for(int i=0;i<2;i++)
+    {
+        std::cout << result[i] << " ";
+    }
+    return 0;
 }
